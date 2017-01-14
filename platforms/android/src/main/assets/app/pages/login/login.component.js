5335e6ad4fe72681f3446f17a654db695af18955
Chanon-SVN <chanon@kmi.tl>
2017-01-14 18:06:32 +0700

    Initial Commit

---
+++ platforms/android/src/main/assets/app/pages/login/login.component.js
@@ -0,0 +1,15 @@
+"use strict";
+var core_1 = require("@angular/core");
+var LoginComponent = (function () {
+    function LoginComponent() {
+    }
+    return LoginComponent;
+}());
+LoginComponent = __decorate([
+    core_1.Component({
+        selector: "my-app",
+        templateUrl: "pages/login/login.component.html"
+    })
+], LoginComponent);
+exports.LoginComponent = LoginComponent;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibG9naW4uY29tcG9uZW50LmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsibG9naW4uY29tcG9uZW50LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSxzQ0FBd0M7QUFPeEMsSUFBYSxjQUFjO0lBQTNCO0lBQTRCLENBQUM7SUFBRCxxQkFBQztBQUFELENBQUMsQUFBN0IsSUFBNkI7QUFBaEIsY0FBYztJQUwxQixnQkFBUyxDQUFDO1FBQ1QsUUFBUSxFQUFFLFFBQVE7UUFDbEIsV0FBVyxFQUFDLGtDQUFrQztLQUMvQyxDQUFDO0dBRVcsY0FBYyxDQUFFO0FBQWhCLHdDQUFjIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHtDb21wb25lbnR9IGZyb20gXCJAYW5ndWxhci9jb3JlXCI7XG5cbkBDb21wb25lbnQoe1xuICBzZWxlY3RvcjogXCJteS1hcHBcIixcbiAgdGVtcGxhdGVVcmw6XCJwYWdlcy9sb2dpbi9sb2dpbi5jb21wb25lbnQuaHRtbFwiXG59KVxuXG5leHBvcnQgY2xhc3MgTG9naW5Db21wb25lbnR7fVxuIl19
---
+++ platforms/android/src/main/assets/app/pages/login/login.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl:"pages/login/login.component.html"
+})
+
+export class LoginComponent{}
