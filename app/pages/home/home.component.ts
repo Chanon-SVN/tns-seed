39b75c8b934044790cb14b58cb2b4d41b8a4f1ed
Chanon-SVN <chanon@kmi.tl>
2017-02-04 13:24:55 +0700

    [ADD] Home Page.

--- app/pages/home/home.component.ts
+++ app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
-import {Component} from "@angular/core";
+import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
+import { DatePicker } from "ui/date-picker";
+var timer = require("timer");
 
 @Component({
   selector: "my-app",
@@ -6,4 +8,17 @@ import {Component} from "@angular/core";
   styleUrls: ["pages/home/home.component.css"]
 })
 
-export class HomeComponent{}
+export class HomeComponent {
+ 
+ public activities:Array<any>;
+ public startDate: Date; 
+ constructor(){
+   this.activities = []; 
+ } 
+ start(){
+  console.log("Start");
+  this.startDate = new Date();
+  this.activities.push({event:"START", time:this.startDate.getHours()+":"+this.startDate.getMinutes(), status:"Running"});
+  console.log(this.startDate.getHours()+" "+this.startDate.getMinutes()+" "+this.startDate.getSeconds());
+ }
+}
